76d024496b62033263050875d47c6e3966f9719d
Noor Naddour <48037617+noornaddour@users.noreply.github.com>
2026-05-04 20:24:39 +1000

    Refactor mobile navigation to use hamburger menu

--- javascript/header-mobile.js
+++ javascript/header-mobile.js
@@ -1,6 +1,13 @@
-const btn = document.getElementById('mobile-btn');
-const menu = document.getElementById('nav-links');
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-btn.addEventListener('click', () => {
-  menu.classList.toggle('show');
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active");
+    }))
